3c42129a4a9613ded03e2a663b51f8ce910537e8
sDEV-eloper <thisissurajdev@gmail.com>
2024-09-20 20:56:56 +0530

    fixes branch model

--- src/models/branchModel.js
+++ src/models/branchModel.js
@@ -1,21 +1,27 @@
 import mongoose from "mongoose";
 
 const branchSchema = new mongoose.Schema({
-    name: { type: String, required:true },
-    location: {
-        latitude: {
-          type: Number,
-        },
-        longitude: { type: true },
-      },
-      address: { type: String },
-      deliveryPartners:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"DeliveryPartner"
-        }
-      ]
-  });
-  
-  const Branch = mongoose.model("Branch", branchSchema);
+  name: { type: String, required: true },
+  location: {
+    latitude: {
+      type: Number,
+      required: true,  // Ensure latitude is required
+    },
+    longitude: { 
+      type: Number,
+      required: true,  // Ensure longitude is required
+    },
+  },
+  address: { type: String },
+  deliveryPartners: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "DeliveryPartner",  // Reference to DeliveryPartner schema
+    },
+  ],
+});
+
+// Model for Branch
+const Branch = mongoose.model("Branch", branchSchema);
+
 export default Branch;
---
+++ src/models/index.js
@@ -0,0 +1,4 @@
+import  {Customer, Branch, DeliveryPartner, Admin} from './userModel.js'
+import Branch from './branchModel.js'
+
+export {Branch, Customer, DeliveryPartner, Admin}
